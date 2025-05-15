d1fff2988f5288f0d8a04012ccf15ce4f147ca81
Selamat Lai <bachiku@hotmail.com>
2025-05-15 15:21:30 +0700

    Add libGL for OpenCV via railway.nix

---
+++ backend/railway.nix
@@ -0,0 +1,12 @@
+{ pkgs }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python312
+    pkgs.python312Packages.pip
+    pkgs.python312Packages.setuptools
+    pkgs.python312Packages.wheel
+
+    pkgs.opencv
+    pkgs.libGL
+  ];
+}
